0243738018c004cf415fa28ed6cbb8d1f9d10891
Doak Covington <doakcovington@protonmail.com>
2019-12-31 02:51:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   #determine if array length is 2
   if array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
+  elsif array.length => 3
+    array[-1] = "and #{array[-1]}"
+    array.join(" ,")  
   end
 end
