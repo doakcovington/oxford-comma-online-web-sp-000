422ffc060c8228fb894b378c7c8a79cdb529aa30
Doak Covington <doakcovington@protonmail.com>
2019-12-31 02:58:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 2
     #converts the array to a string and puts "and" as between the 2 elements
     array.join(" and ")
-  #else if array length is greater than or equal to 3    
+  #else if array length is greater than or equal to 3
   elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
     array.join(", ")
